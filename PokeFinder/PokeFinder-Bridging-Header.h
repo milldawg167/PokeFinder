11cdf4606e953182ac1c339c0e0a54c345d6b9f5
Andrew Miller <milldawg@github.com>
2018-05-15 17:10:40 +0100

    Finished the PokeFinder app

---
+++ PokeFinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  PokeFinder
+//
+//  Created by Andrew Miller on 15/05/2018.
+//  Copyright © 2018 Andrew Miller. All rights reserved.
+//
+
+#import "Geofire.h"
